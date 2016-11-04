932c62de754b26e2b0923c8a40fd42f1608709eb
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-04 22:15:10 +0000

    Destroy grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#destroy action" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryGirl.create(:gram)
+      delete :destroy, id: gram.id
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should return 404 if no gram with specified id" do
+      delete :destroy, id: 'MEOW'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
   describe "grams#update action" do
     it "should allow users to successfully update grams" do
       gram = FactoryGirl.create(:gram, message: "Initial Value")
